080005cdd7025cc60db6e6747b9823d4de772dd6
opengeers <opengeers.com@gmail.com>
2017-12-19 02:52:30 +0530

    create new user

--- src/actions/AuthActions.js
+++ src/actions/AuthActions.js
@@ -8,12 +8,12 @@ export const fieldChange = (payload) => {
 };
 
 export const SignUpAction = (payload) => {
-  console.log(payload);
   return (dispatch) => {
-    const email = 'subu@gmail.com';
-    const password = '123456';
+    console.log(payload);
+    const email = payload.value.othersInfo.email;
+    const password = payload.value.othersInfo.password;
     firebase.auth().createUserWithEmailAndPassword(email, password)
-      .then(user => signUpSuccess(dispatch, user))
+      .then(user => signUpSuccess(dispatch, user, payload))
       .catch((error) => {
         signUpFail(dispatch, error);
       });
@@ -21,12 +21,27 @@ export const SignUpAction = (payload) => {
 };
 
 // Signup success function
-const signUpSuccess = (dispatch, user) => {
+const signUpSuccess = (dispatch, user, payload) => {
+  // [Function] to Create a user in database
+  createNewUser(user, payload);
+
+  // dispatch an action
   dispatch({
     type: 'SIGNUP_SUCCESS',
     payload: user
   });
 };
+
+function createNewUser(user, payload) {
+  const userData = {
+    role: payload.value.role,
+    email: payload.value.othersInfo.email,
+    password: payload.value.othersInfo.password,
+    firstName: payload.value.othersInfo.firstName,
+    lastName: payload.value.othersInfo.lastName,
+  };
+  firebase.database().ref('users/').push(userData);
+}
 
 // signup fail function
 const signUpFail = (dispatch, error) => {
